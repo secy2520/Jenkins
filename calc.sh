f2bfbee0490360ad09df7569071bdff5c24a9e69
secy2520 <82306041+secy2520@users.noreply.github.com>
2024-02-08 19:17:31 +0530

    n

--- calc.sh
+++ calc.sh
@@ -1,17 +1,18 @@
 # !/bin/bash
 # A simple calculator shell program
-
+a=$1
+b=$2
 echo "1. Addition"
 echo "2. Subtraction"
 echo "3. Multiplication"
 echo "4. Division"
 
 echo -n "Enter First Number: "
-read a
+
 echo -n "Enter Second Number: "
 read b
 echo -n "Enter the Choice: "
-read ch
+ch = $3
 
 case $ch in
    1) res=`expr $a + $b`
